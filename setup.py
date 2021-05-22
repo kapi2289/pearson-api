7bb5f3e0c5d40028f3b7afa0ee6ffb1464a6c4c4
Kacper Ziubryniewicz <kapi2289@gmail.com>
2021-05-23 00:55:37 +0200

    Bump version to v1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pearson-api',
-      version='1.0',
+      version='1.1',
       description='Pearson API',
       author='Kacper Ziubryniewicz',
       author_email='kapi2289@gmail.com',
